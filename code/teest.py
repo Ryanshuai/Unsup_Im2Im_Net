fca9cf781643aa59bbbe09b661e88b1e38f68485
Ryan <1018718155@qq.com>
2017-12-11 10:13:28 +0800

    1

--- code/teest.py
+++ code/teest.py
@@ -7,31 +7,33 @@ from scipy.io import loadmat
 
 current_dir = os.getcwd()
 parent_path = os.path.dirname(current_dir)
-
 svhn_dir = os.path.join(parent_path, 'SVHN-data')
-
 svhn_train_data = loadmat(svhn_dir + '/train_32x32.mat')
 
-
-print(type(svhn_train_data['X']))
-print(type(svhn_train_data['y']))
+XXX = svhn_train_data['X'].transpose((3, 0, 1, 2))
+yyy = svhn_train_data['y']
 
 #data processing func used in map
-def preprocessing(filename):
-    image_string = tf.read_file(filename)
-    image = tf.image.decode_png(image_string)
-    image = tf.image.resize_images(image, [128, 128])
-    image.set_shape([128, 128, 3])
+def preprocessing(image, label):
+    image = tf.image.resize_images(image, [28, 28])
+    image.set_shape([28, 28, 3])
     # image = tf.image.random_flip_left_right(image)
     # image = tf.image.random_brightness(image, max_delta=0.1)
     # image = tf.image.random_contrast(image, lower=0.9, upper=1.1)
     image = tf.cast(image, tf.float32)
-    image = image / 255.0
-    return image
+    #image = image / 255.0
+    return image, label
 
-#make dataset
-dataset = tf.data.Dataset.from_tensor_slices(tensor_image_paths)
+dataset = tf.data.Dataset.from_tensor_slices((XXX, yyy))
 #dataset = dataset.repeat(32)
 dataset = dataset.map(preprocessing)
 # dataset = dataset.shuffle(3200)
-dataset = dataset.batch(BS)
+dataset = dataset.batch(1)
+
+
+iterator = dataset.make_initializable_iterator()
+
+with tf.Session() as sess:
+    sess.run(iterator.initializer)
+    test_data = sess.run(iterator.get_next())
+    print(test_data[0].shape)
